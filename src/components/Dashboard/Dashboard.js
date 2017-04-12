3aec9c8a977268197bd8c43450a173c1c5e27740
Alex Helwani <alex.helwani@interadcorp.com>
2017-04-12 17:20:01 -0400

    initial commit

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './Dashboard.scss'
+
+export const Dashboard = (props) => (
+  <div>
+    <h2 className={classes.dashboardContainer}>
+      Dashboard visits:
+      {' '}
+      <span className={classes['dashboard--green']}>
+        {props.dashboard}
+      </span>
+    </h2>
+  </div>
+)
+
+Dashboard.propTypes = {
+  dashboard: React.PropTypes.number.isRequired,
+}
+
+export default Dashboard
